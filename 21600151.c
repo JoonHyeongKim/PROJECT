67685d8f9d24d23d9fde012c19afa025330ffff3
JoonHyeongKim <49176227+JoonHyeongKim@users.noreply.github.com>
2019-04-05 00:59:47 +0900

    Update 21600151.c

--- 21600151.c
+++ 21600151.c
@@ -3,25 +3,13 @@
 #include "21600151.h"
 #define MAX_SIZE 2048
 
-int reverse_digits(int n){
-	int number[MAX_SIZE];
-	int result=0;
-	int count;
-
-	for(int i=0;i<MAX_SIZE;i++){
-		number[i]=n%10;
-		if(n==0 && n/10==0)
-			break;
-	n/=10;
-	count =i;
-	}
-	
-	for(int i=0;i<count+1;i++){
-		result*=10;
-		result+=number[i];
-	}
-	
-	return result;
+boolean isPrime(int n){
+	if(n<=1)
+		return false;
+	for(int i=2;i<n;i++)
+		if(n%i==0)
+			return false;
+	return true;
 
 }
 
